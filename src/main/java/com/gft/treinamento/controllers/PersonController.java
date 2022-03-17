28da427ccdbb4cd0ef89fe776ea72d5fe80529c8
rkai <ronald.liboni@gft.com>
2022-03-16 21:01:47 -0300

    aprendizado do dia

---
+++ src/main/java/com/gft/treinamento/controllers/PersonController.java
@@ -0,0 +1,71 @@
+package com.gft.treinamento.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.gft.treinamento.entities.Person;
+import com.gft.treinamento.services.PersonService;
+
+@Controller
+@RequestMapping("/person")
+public class PersonController {
+	
+	@Autowired
+	PersonService personService;
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public ModelAndView getPerson(@RequestParam Long id) {
+		
+		ModelAndView mv = new ModelAndView("person.html");
+		
+		try {
+			Person person = personService.getPerson(id);
+			mv.addObject("person", person);
+		}catch (Exception e) {
+			mv.addObject("message",e.getMessage());
+		}
+		
+		return mv;
+	}
+	
+	@RequestMapping(method = RequestMethod.GET, path = "/new")
+	public ModelAndView forPerson() {
+		
+		ModelAndView mv = new ModelAndView("formperson.html");
+		mv.addObject("person", new Person());
+		
+		return mv;
+	}
+	
+	@RequestMapping(method = RequestMethod.GET, path="/edit")
+	public ModelAndView editPerson(@RequestParam Long id) {
+		ModelAndView mv = new ModelAndView("formperson.html");
+		
+		try {
+			Person person = personService.getPerson(id);
+			mv.addObject("person", person);
+		} catch(Exception e) {
+			e.printStackTrace();
+		}		
+		return mv;
+	}
+	
+	@RequestMapping(method = RequestMethod.POST )
+	public ModelAndView newPerson(Person person) {
+	
+		ModelAndView mv = new ModelAndView("person.html");
+		
+		person = personService.savePerson(person);
+		
+		mv.addObject("person", person);
+		
+		return mv;
+	}
+	
+	
+	
+}
---
+++ src/main/java/com/gft/treinamento/services/PersonService.java
@@ -0,0 +1,40 @@
+package com.gft.treinamento.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.gft.treinamento.entities.Person;
+import com.gft.treinamento.repositories.PersonRepository;
+
+@Service
+public class PersonService {
+	
+	@Autowired
+	PersonRepository personRepository;
+	
+	public Person CreatePerson() {
+		Person person = new Person();
+		person.setName("Rony");
+		person.setAge(26);
+		person.setStatus(Boolean.TRUE);
+		return person;
+	}
+	
+	public Person savePerson (Person person) {
+		return personRepository.save(person);
+	}
+	
+	public Person getPerson(Long id) throws Exception {
+		
+		Optional<Person> person = personRepository.findById(id);
+		
+		if (person.isEmpty()) {
+			throw new Exception("Person not found!");
+		}
+		
+		return person.get();
+	}
+	
+}
